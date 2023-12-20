5c19a8b162ca93839d838b48aeea9dc120958785
GearShocky <gearshockmirasu@gmail.com>
2023-12-20 15:09:50 +0600

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom'
 import './App.css';
+import logoImage from './media/bytelogo.png';	
+import logoBg from './media/Logoback.png';
+import Home from './pages/Home'
+import Games from './pages/games/Malware'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+			<img className = "logoBg" src={logoBg} alt=""/>
+			<img className = "logo" src={logoImage} alt="GearShocky"/>
+			<nav>
+				<ul>
+					<li>
+						<link to="/">Home</link>
+					</li>
+                    <li>
+						<link to="/games/malware">Games</link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+    return <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/games/malware" element={<Games/>}/>
+    </Routes>
+
 }
 
-export default App;
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+
+
+		<div className="container">
+
+		</div>
+
+		<div class="box-transparent-1">
+
+		</div>
+    </div>
+  );
+}
+
+export default Home;
