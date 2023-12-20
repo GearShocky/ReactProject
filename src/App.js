05af8e6015ce9e2cfe77fade8e890bc92c39d65e
GearShocky <gearshockmirasu@gmail.com>
2023-12-20 15:41:41 +0600

    Figured out routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import {Route, Routes} from 'react-router-dom'
-import './App.css';
-import logoImage from './media/bytelogo.png';	
-import logoBg from './media/Logoback.png';
 import Home from './pages/Home'
 import Games from './pages/games/Malware'
 
 function App() {
-    <header>
-			<img className = "logoBg" src={logoBg} alt=""/>
-			<img className = "logo" src={logoImage} alt="GearShocky"/>
-			<nav>
-				<ul>
-					<li>
-						<link to="/">Home</link>
-					</li>
-                    <li>
-						<link to="/games/malware">Games</link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-
     return <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/games/malware" element={<Games/>}/>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/games/malware" element={<Games/>}/>
     </Routes>
-
 }
 
 
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,36 +1,31 @@
 import { Outlet, Link } from "react-router-dom";
+import './layout.css';
 import logoImage from '../media/bytelogo.png';	
 import logoBg from '../media/Logoback.png';
 
 const Layout = () => {
   return (
-    <>
+    <div className="layout">
       <header>
 			<img className = "logoBg" src={logoBg} alt=""/>
 			<img className = "logo" src={logoImage} alt="GearShocky"/>
 			<nav>
 				<ul>
 					<li>
-						<a to="/"> Home </a>
+						<Link to="/">Home</Link>
 					</li>
-					<li>
-						<a href="about.html"> About </a>
+          <li>
+						<Link to="/">About</Link>
 					</li>
-					<li>
-						<a to="/games"> Games </a>
-					</li>
-					<li>
-						<a href="about.html"> Blog </a>
-					</li>
-					<li> 
-						<a href="booking.html"> Contact </a>
+          <li>
+						<Link to="/games/malware">Games</Link>
 					</li>
 				</ul>
 			</nav>
 		</header>
 
       <Outlet />
-    </>
+    </div>
   )
 };
 
